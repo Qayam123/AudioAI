e37d0af801fe3330e2fd7accc9643c249cbbad32
Qayamul Haque <113770481+Qayam123@users.noreply.github.com>
2024-05-30 23:58:43 +0530

    Add files via upload

---
+++ routine.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+sudo apt autoremove
+sudo apt update
+sudo apt upgrade
+mv *.pdf pdf
+mv *.xlsx excel
+mv *.docx doc
+mv *.py py
+mv *.ipynb ipynb
+mv *.gz tar
+mv *.xz tar
+mv *.csv csv
+mv *.zip zip
+mv *.jpg *.png *.svg Images
+mv *.deb deb
---
+++ textinput2audio.py
@@ -0,0 +1,36 @@
+# Import the required module for text to speech conversion
+from gtts import gTTS
+
+# Import pygame for playing the converted audio
+import pygame
+import time
+
+# The text that you want to convert to audio
+mytext = input("Enter your text")
+
+# Language in which you want to convert
+language = 'en'
+
+# Passing the text and language to the engine,
+# here we have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file named
+# welcome
+myobj.save("welcome2.mp3")
+
+# Initialize the pygame mixer
+pygame.mixer.init()
+
+# Load the mp3 file
+pygame.mixer.music.load("welcome2.mp3")
+
+# Play the loaded mp3 file
+pygame.mixer.music.play()
+
+# Keep the script running until the audio finishes playing
+while pygame.mixer.music.get_busy():
+    time.sleep(1)
+
